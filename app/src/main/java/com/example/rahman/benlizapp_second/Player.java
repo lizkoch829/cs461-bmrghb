5fc9fc84446ce5a6008b88f270d83d106b16505d
Liz <elizabethgkoch@gmail.com>
2018-11-04 23:00:59 -0600

    added player xml

--- app/src/main/java/com/example/rahman/benlizapp_second/Player.java
+++ app/src/main/java/com/example/rahman/benlizapp_second/Player.java
@@ -14,48 +14,55 @@ public class Player {
     int position;
     int seasons;
     int base_salary;
+    int gbs;
     int prorated_bonus;
     int roster_bonus;
     int workout_bonus;
     int cap_number;
-    int projected_dm;
-    int june1_projected_dm;
-    int trade_projected_dm;
-    int next_year_trade_projected_dm;
+    int cut_dm;
+    int june1_cut_dm;
+    int trade_dm;
     int restruct_dm;
     int extend_dm;
+    int cut_gain_loss;
     int june1_cut_dm_gain_loss;
     int trade_dm_gain_loss;
     int restructure_dm_gain_loss;
-    int extend_dm_gain_loss;
+    int extended_gain_loss;
+    int repeated_dm_gain_loss;
+
+    //Fname,Lname,Pos,AS,BaseSal,GBS,ProBonus,RosBonus,WorkBonus,CapNum,CUTDM,J1CUTDM,TradeDM,
+    // RestuctDM,XtdDM,CUTGainLoss,J1CGainLoss,TradeGainLoss,RestructGL,XtdGainLoss,REPEATEDCutGainLoss
 
     public Player( String first_name,String last_name,int position, int seasons, int base_salary,
-                   int prorated_bonus,int roster_bonus,int workout_bonus,int cap_number,
-                   int projected_dm,int june1_projected_dm, int trade_projected_dm,
-                   int next_year_trade_projected_dm,int restruct_dm, int extend_dm,
-                   int june1_cut_dm_gain_loss, int trade_dm_gain_loss, int restructure_dm_gain_loss,
-                    int extend_dm_gain_loss)
+                   int gbs, int prorated_bonus,int roster_bonus,int workout_bonus,int cap_number,
+                   int cut_dm,int june1_cut_dm, int trade_dm,int restruct_dm,int extend_dm,
+                   int cut_gain_loss, int june1_cut_dm_gain_loss, int trade_dm_gain_loss,
+                   int restructure_dm_gain_loss, int extended_gain_loss,int repeated_dm_gain_loss)
     {
         this.first_name=first_name;
         this.last_name=last_name;
         this.position=position;
         this.seasons=seasons;
         this.base_salary=base_salary;
+        this.gbs=gbs;
         this.prorated_bonus=prorated_bonus;
         this.roster_bonus=roster_bonus;
         this.workout_bonus=workout_bonus;
         this.cap_number=cap_number;
-        this.projected_dm=projected_dm;
-        this.june1_projected_dm=june1_projected_dm;
-        this.trade_projected_dm=trade_projected_dm;
-        this.next_year_trade_projected_dm=next_year_trade_projected_dm;
+        this.cut_dm=cut_dm;
+        this.june1_cut_dm=june1_cut_dm;
+        this.trade_dm=trade_dm;
         this.restruct_dm=restruct_dm;
         this.extend_dm=extend_dm;
+        this.cut_gain_loss=cut_gain_loss;
         this.june1_cut_dm_gain_loss=june1_cut_dm_gain_loss;
         this.trade_dm_gain_loss=trade_dm_gain_loss;
         this.restructure_dm_gain_loss=restructure_dm_gain_loss;
-        this.extend_dm_gain_loss=extend_dm_gain_loss;
+        this.extended_gain_loss=extended_gain_loss;
+        this.repeated_dm_gain_loss=repeated_dm_gain_loss;
     }
+
 
 
 
@@ -80,6 +87,10 @@ public class Player {
     {
         return base_salary;
     }
+    public int get_gbs()
+    {
+        return gbs;
+    }
     public int get_prorated_bonus()
     {
         return prorated_bonus;
@@ -96,21 +107,17 @@ public class Player {
     {
         return cap_number;
     }
-    public int get_projected_dm()
+    public int get_cut_dm()
     {
-        return projected_dm;
+        return cut_dm;
     }
-    public int get_june1_projected_dm()
+    public int get_june1_cut_dm()
     {
-        return june1_projected_dm;
+        return june1_cut_dm;
     }
-    public int get_trade_projected_dm()
+    public int get_trade_dm()
     {
-        return trade_projected_dm;
-    }
-    public int get_next_year_trade_projected_dm()
-    {
-        return next_year_trade_projected_dm;
+        return trade_dm;
     }
     public int get_restruct_dm()
     {
@@ -119,6 +126,10 @@ public class Player {
     public int get_extend_dm()
     {
         return extend_dm;
+    }
+    public int get_cut_gain_loss()
+    {
+        return cut_gain_loss;
     }
     public int get_june1_cut_dm_gain_loss()
     {
@@ -132,13 +143,13 @@ public class Player {
     {
         return restructure_dm_gain_loss;
     }
-    public int get_extend_dm_gain_loss()
+    public int get_extended_gain_loss()
     {
-        return extend_dm_gain_loss;
+        return extended_gain_loss;
     }
-
-    //third activity
-    //name, position, #seasons, base salary, guaranteed salary, cut spinner (june 1 cut, extension,
-    // restructure ,trade)
+    public int get_repeated_dm_gain_loss()
+    {
+        return repeated_dm_gain_loss;
+    }
 
 }
--- app/src/main/java/com/example/rahman/benlizapp_second/PlayerActivity.java
+++ app/src/main/java/com/example/rahman/benlizapp_second/PlayerActivity.java
@@ -2,13 +2,37 @@ package com.example.rahman.benlizapp_second;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class PlayerActivity extends Activity {
+
+public class PlayerActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        Spinner spinner=findViewById(R.id.player_spinner);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.player_options,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text= parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
